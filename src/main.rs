bd1098ebaed7087826846695b3df8f80ff1af2d8
risolij <risolij@gmail.com>
2021-10-28 21:47:23 -0700

    let's start the base tui

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
-fn main() {
-    let a = 4;
-    println!("{}", a);
+use std::io;
+use termion::raw::IntoRawMode;
+use tui::Terminal;
+use tui::backend::TermionBackend;
+use tui::widgets::{Widget, Block, Borders};
+
+fn main() -> Result<(), io::Error> {
+    let stdout = io::stdout().into_raw_mode()?;
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|term| {
+	let size = term.size();
+	let block = Block::default()
+	    .title("Block")
+	    .borders(Borders::ALL);
+	term.render_widget(block, size);
+    })?;
+    
+
+    Ok(())
 }
