d7f725d0c10b2afe1818a50355ce412bf34ce91b
risolij <risolij@gmail.com>
2021-10-30 22:34:21 -0700

    updating a nav template

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 use yew::prelude::*;
 
+
 enum Msg {
     AddOne,
 }
 
+
 struct Model {
-    // `ComponentLink` is like a reference to a component.
-    // It can be used to send messages to the component
     link: ComponentLink<Self>,
     value: i64,
 }
 
+
 impl Component for Model {
     type Message = Msg;
     type Properties = ();
+
 
     fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
         Self {
@@ -22,30 +24,46 @@ impl Component for Model {
         }
     }
 
+
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
             Msg::AddOne => {
                 self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
                 true
             }
         }
     }
 
+
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        // Should only return "true" if new properties are different to
-        // previously received properties.
-        // This component has no properties so we will always return "false".
         false
     }
 
+
     fn view(&self) -> Html {
         html! {
-            <div>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
-                <p>{ self.value }</p>
-            </div>
+	    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<a class="navbar-brand" href="#">{ "Navbar" }</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">{ "Home" }<span class="sr-only"></span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">{ "Features" }</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">{ "Pricing" }</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#">{ "Disabled" }</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         }
     }
 }
